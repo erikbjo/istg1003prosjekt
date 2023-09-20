faabd68225dd3be0ee60ce6e41387880a4a75e08
Erik Bjørnsen <bjornsen.erik@gmail.com>
2023-09-20 20:15:41 +0200

    added titles to plot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import openpyxl # needed for read_excel
+import openpyxl  # needed for read_excel
 import matplotlib.pyplot as plt
 from scipy import stats
 
@@ -7,12 +7,19 @@ df = pd.read_excel('skostr_hoyde.xlsx')
 
 slope, intercept, r, p, std_err = stats.linregress(df['skostr'], df['hoyde'])
 
+
 def myfunc(x):
-  return slope * x + intercept
+    return slope * x + intercept
+
 
 mymodel = list(map(myfunc, df['skostr']))
 
 plt.scatter(df['skostr'], df['hoyde'])
-plt.plot(df['skostr'], mymodel)
+plt.plot(df['skostr'], mymodel, color='red')
+plt.xlabel('Sko størrelse')
+plt.ylabel('Høyde')
+plt.title('Sko størrelse mot høyde')
+plt.legend(['y={:.2f}x+{:.2f}'.format(slope, intercept)])
+plt.grid()
 plt.savefig('skostr_hoyde.pdf')
 plt.show()
