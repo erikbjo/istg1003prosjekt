cb26fbfa48aa9f625b02a6e077f8d2363c035b35
Erik Bjørnsen <bjornsen.erik@gmail.com>
2023-09-20 19:52:18 +0200

    initian main and readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# ISTG1003 Forprosjekt
+# ISTG1003 Forprosjekt Gruppe 11
+
+---
+
+## How to run the project
+```bash
+python main.py
+```
+
+## Members
+- [Martin Morisbak](mailto:martmori@ntnu.no) (Project Leader)
+- [Eskil Alstad](mailto:eskil.alstad@ntnu.no)
+- [Erik Bjørnsen](mailto:erbj@ntnu.no)
+- [Simon Houmb](mailto:simon.h.houmb@ntnu.no)
+- [Erlend Rømo](mailto:erlenrom@ntnu.no)
+- [Arthur Thorkildsen](mailto:arthur.b.thorkildsen@ntnu.no)
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import pandas as pd
-import openpyxl
+import openpyxl # needed for read_excel
+import matplotlib.pyplot as plt
+from scipy import stats
 
 df = pd.read_excel('skostr_hoyde.xlsx')
 
-print(df)
+slope, intercept, r, p, std_err = stats.linregress(df['skostr'], df['hoyde'])
+
+def myfunc(x):
+  return slope * x + intercept
+
+mymodel = list(map(myfunc, df['skostr']))
+
+plt.scatter(df['skostr'], df['hoyde'])
+plt.plot(df['skostr'], mymodel)
+plt.show()
