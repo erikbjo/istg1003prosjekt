d03bc2bdf9a82d7c9d87d87845d7ac88c145ee1f
erlend <erlenrom@stud.ntnu.no>
2023-11-04 16:58:21 +0100

    Modiefied program.py, and now containing all elements of the dataframe.

--- Oppgaver/program.py
+++ Oppgaver/program.py
@@ -5,7 +5,7 @@ import pandas as pd
 def getDataFrame(path):
     df = pd.read_csv(path, sep = ",", encoding = "latin1")
 
-    df_actual = df[["Theme", "Price", "Pieces", "Pages", "Minifigures"]]
+    df_actual = df[["Theme", "Price", "Pieces", "Pages", "Minifigures", "Unique_Pieces"]]
     df_actual = df_actual.dropna()
 
     df_actual["Theme"] = df_actual["Theme"].astype(str)
@@ -29,9 +29,26 @@ def main():
     selector = "Star Wars"
     work_df = getSelectionDataFrame(df, selector)
     
-    printContentInDataFrame(work_df)
+    #printContentInDataFrame(work_df)
     
-    correlation_a = computations.getCorrelationRelatedToElement(work_df, "Minifigures")
-    print(correlation_a)
+    correlations_array = []
+    
+    correlation_pieces = computations.getCorrelationRelatedToElement(work_df, "Pieces")
+    correlation_pages = computations.getCorrelationRelatedToElement(work_df, "Pages")
+    correlation_minifigures = computations.getCorrelationRelatedToElement(work_df, "Minifigures")
+    #correlation_unique_pieces = computations.getCorrelationRelatedToElement(work_df, "Unique_Pieces")
+    
+    pieces = correlations.Correlations("Pieces", "Price", correlation_pieces)
+    pages = correlations.Correlations("Pages", "Price", correlation_pages)
+    minifigures = correlations.Correlations("Minifigures", "Price", correlation_minifigures)
+    #unique_pieces = correlations.Correlations("Unique_Pieces", "Price", correlation_unique_pieces)
+    
+    correlations.appendToArray(correlations_array, pieces)
+    correlations.appendToArray(correlations_array, pages)
+    correlations.appendToArray(correlations_array, minifigures)
+    #correlations.appendToArray(correlations_array, unique_pieces)
+    
+    for item in correlations_array:
+        item.printContent()
 
 main()
